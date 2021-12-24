ec8b79aae13d4fb49bea1d32e5f8a4fb534f652e
warris2 <92058571+warris2@users.noreply.github.com>
2021-12-24 11:07:28 -0400

    Add files via upload

---
+++ database/migrations/2021_11_14_210640_create_convocatorias_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConvocatoriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('convocatorias', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tit_conv')->required();
+            $table->string('desc_conv')->required();
+            $table->timestamps();
+            $table->date('f_fin')->required();
+            
+
+            $table->foreignId('id_consultor')->nullable()->constrained('users')->cascadeOnUpdate()->nullOnDelete();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('convocatorias');
+    }
+}
