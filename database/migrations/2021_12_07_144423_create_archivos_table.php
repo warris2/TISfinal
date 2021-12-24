41ff9368ac3a257670f45c87f57311c02321099b
warris2 <92058571+warris2@users.noreply.github.com>
2021-12-24 11:14:45 -0400

    Add files via upload

---
+++ database/migrations/2021_12_07_144423_create_archivos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArchivosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('archivos', function (Blueprint $table) {
+            $table->id();
+            $table->string('tipo_1a')->nullable();
+            $table->string('tipo_2a')->nullable();
+            $table->string('tipo_3a')->nullable();
+            $table->string('tipo_4a')->nullable();
+            $table->string('tipo_1b')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('archivos');
+    }
+}
